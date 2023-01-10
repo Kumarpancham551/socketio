47949c48af44426dd5abca45abd96d825da8dd88
Pancham Kumar <kumarpancham551@gmail.com>
2023-01-10 17:42:00 +0530

    s

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const socket =  require('socket.io');
+const io = socket(http);
+
+
+/**
+ * Rendering html
+ */
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+})
+
+
+
+http.listen(8080,()=>{
+    console.log("Application started");
+})
